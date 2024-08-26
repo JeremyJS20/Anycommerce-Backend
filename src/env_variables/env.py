8dd9d879ba301b8e0c98038bda77b1dccfc05054
JeremyJS20 <55357712+JeremyJS20@users.noreply.github.com>
2024-08-26 09:07:12 -0400

    Update env.py

--- src/env_variables/env.py
+++ src/env_variables/env.py
@@ -2,6 +2,17 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class EnvVariables(BaseSettings):
+    port: str
+    host: str
+    mongodb_connection_string: str
+    mongodb_database: str
+    auth_secret_key: str
+    origin: str
+    stripe_secret_key: str
+    countries_api_url: str
+    countries_api_key: str
+    currency_convertion_api_url: str
+
     model_config = SettingsConfigDict(env_file=".env", extra="allow")
 
 
